33424033dc697703eb723a293ff840336d58b27d
yyaannn <g853520ck@gmail.com>
2025-04-28 21:16:48 +0800

    feat: The Nav Mesh Surface can be built on the runtime generated room and the Nav Mesh Agent can walk on it

--- Naruto-MR/Assets/Scripts/MRUKManager.cs
+++ Naruto-MR/Assets/Scripts/MRUKManager.cs
@@ -2,11 +2,18 @@ using UnityEngine;
 using Meta.XR.MRUtilityKit;
 using System.Collections.Generic;
 using TMPro;
+using System.Collections;
+using UnityEditor.Search;
+using Unity.AI.Navigation;
 
 public class MRUKManager : MonoBehaviour
 {
     public MRUK mruk;
     public OVRInput.Controller controller;
+
+    public GameObject floor;
+    public NavMeshSurface surface;
+    public GameObject NPC;
 
     private bool sceneHasBeenLoaded;
     public MRUKRoom currentRoom;
@@ -14,6 +21,8 @@ public class MRUKManager : MonoBehaviour
     public List<GameObject> anchorObjectsCreated = new();
 
     private bool SceneAndRoomInfoAvailable => currentRoom != null && sceneHasBeenLoaded;
+
+    public bool isBuilt = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,28 +34,38 @@ public class MRUKManager : MonoBehaviour
     void Update()
     {
         // Press the right trigger to create or destroy anchor objects showing the anchor index
-        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) && SceneAndRoomInfoAvailable)
+        //if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) && SceneAndRoomInfoAvailable)
+        //{
+        //    if (anchorObjectsCreated.Count == 0)
+        //    {
+        //        foreach (var anchor in currentRoom.Anchors)
+        //        {
+        //            var gameObject = Instantiate(new GameObject(), Vector3.zero, Quaternion.identity, anchor.transform);
+        //            gameObject.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
+        //            gameObject.transform.localScale = Vector3.one * 0.1f;
+        //            gameObject.AddComponent<TextMeshPro>();
+        //            gameObject.GetComponent<TextMeshPro>().text = anchor.transform.position.ToString();
+        //            anchorObjectsCreated.Add(gameObject);
+        //        }
+        //    }
+        //    else
+        //    {
+        //        foreach (var anchorObject in anchorObjectsCreated)
+        //        {
+        //            Destroy(anchorObject);
+        //        }
+        //        anchorObjectsCreated.Clear();
+        //    }
+        //}
+
+        if (!isBuilt)
         {
-            if (anchorObjectsCreated.Count == 0)
+            MeshFilter mesh = FindAnyObjectByType<MeshFilter>();
+            if (mesh)
             {
-                int i = 0;
-                foreach (var anchor in currentRoom.Anchors)
-                {
-                    var gameObject = Instantiate(new GameObject(), Vector3.zero, Quaternion.identity, anchor.transform);
-                    gameObject.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
-                    gameObject.transform.localScale = Vector3.one * 0.1f;
-                    gameObject.AddComponent<TextMeshPro>();
-                    gameObject.GetComponent<TextMeshPro>().text = i.ToString();
-                    anchorObjectsCreated.Add(gameObject);
-                    i++;
-                }
-            } else
-            {
-                foreach (var anchorObject in anchorObjectsCreated)
-                {
-                    Destroy(anchorObject);
-                }
-                anchorObjectsCreated.Clear();
+                Debug.Log("Meshes are ready!");
+                isBuilt = true;
+                BuildNavMesh();
             }
         }
     }
@@ -71,5 +90,13 @@ public class MRUKManager : MonoBehaviour
     {
         currentRoom = room;
         Debug.Log($"{nameof(MRUKManager)} room was bound to current room");
+    }
+
+    public void BuildNavMesh()
+    {
+        floor = GameObject.Find("FLOOR_EffectMesh");
+        surface.BuildNavMesh();
+        NPC.SetActive(true);
+        NPC.transform.position = floor.transform.position + new Vector3(2, 10, 2);
     }
 }
--- Naruto-MR/Assets/Scripts/NPCController.cs
+++ Naruto-MR/Assets/Scripts/NPCController.cs
@@ -1,63 +1,108 @@
 using UnityEngine;
 using UnityEngine.AI;
 using Unity.AI.Navigation;
+using Meta.XR.MRUtilityKit;
+using NUnit.Framework;
 
 public class NPCController : MonoBehaviour
 {
-
-    enum NinJutsu {
-
-    }
-
     public Transform player;
     public NavMeshSurface surface;
-    public float attackDistance;
+    public float taijutsuThreshold;
+    public float CloseRangeThreshold;
+    public int blood = 30;
 
     private NavMeshAgent agent;
     private Animator animator;
 
-    NinJutsu[] ninJutsuList = {}; 
-    public int currentNinJutsu;
+    public Ninjutsu Fireball = new Ninjutsu(NinjutsuName.Fireball, 3, ElementType.Fire);
+    public Ninjutsu Raikiri = new Ninjutsu(NinjutsuName.Raikiri, 3, ElementType.Lightning);
+    public Ninjutsu GalePalm = new Ninjutsu(NinjutsuName.GalePalm, 3, ElementType.Wind);
+    public Ninjutsu RockShelterCollapse = new Ninjutsu(NinjutsuName.RockShelterCollapse, 3, ElementType.Earth);
+    public Ninjutsu Waterfall = new Ninjutsu(NinjutsuName.Waterfall, 3, ElementType.Water);
+    public Ninjutsu PhoenixFire = new Ninjutsu(NinjutsuName.PhoenixFire, 3, ElementType.Fire);
+    public Ninjutsu Rasengan = new Ninjutsu(NinjutsuName.Rasengan, 2, ElementType.None);
+    // There are still other physical attacks needed to be add ...
+
+    public int currentNinjutsu;
 
     public MRUKManager mrukManager;
+    public MRUKRoom currentRoom;
+
+    public float coolDown = 5f;
+    private float currentCoolDown;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
-        //animator = GetComponent<Animator>();
-
-        if (agent == null)
-        {
-            Debug.LogError("NavMeshAgent is missing!");
-        }
-
-        if (player == null)
-        {
+        if (!player)
             player = Camera.main.transform;
-        }
 
-        if (surface == null)
-        {
+        if (!surface)
             Debug.LogError("NavMeshSurface is missing!");
-        }
+
+        agent = GetComponent<NavMeshAgent>();
+        if (!agent)
+            Debug.LogError("NavMeshAgent is missing!");
+
+        //animator = GetComponent<Animator>();
+        //if (!animator)
+        //    Debug.Log("Animator is missing!");
+
+        currentRoom = mrukManager.currentRoom;
         // surface.BuildNavMesh();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float distance = Vector3.Distance(agent.transform.position, player.position);
-        if (distance < attackDistance)
+        //float distance = Vector3.Distance(agent.transform.position, player.position);
+        //if (distance < taijutsuThreshold)
+        //{
+        //    PerformTaijutsu();
+        //    currentCoolDown = coolDown;
+        //}
+        //else
+        //{
+        //    agent.isStopped = false;
+        //    //animator.SetBool("Attack", false);
+        //    agent.destination = player.position;
+        //}
+
+        //currentCoolDown -= Time.deltaTime;
+        //if (currentCoolDown <= 0f)
+        //{
+        //    CastNinjutsu();
+
+        //}
+        agent.destination = player.position;
+    }
+
+    //void OnCollisionEnter(Collision collision)
+    //{
+    //    // If the NPC is hit by the player of the effect of ninjutsu
+    //    if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Effect"))
+    //    {
+    //        // ...
+    //    }
+    //}
+
+    void CastNinjutsu()
+    {
+        float distance = Vector3.Distance(transform.position, player.transform.position);
+        if (distance <= CloseRangeThreshold)
         {
-            agent.isStopped = true;
-            //animator.SetBool("Attack", true);
+            // Close range ninjutsu
         } else
         {
-            agent.isStopped = false;
-            //animator.SetBool("Attack", false);
-            agent.destination = player.position;
+            // Long range ninjutsu
         }
+    }
+
+    void PerformTaijutsu()
+    {
+        agent.isStopped = true;
+        //animator.SetBool("Attack", true);
     }
 
     //private void OnAnimatorMove()
@@ -67,9 +112,39 @@ public class NPCController : MonoBehaviour
     //        agent.speed = (animator.deltaPosition / Time.deltaTime).magnitude;
     //    }
     //}
+}
 
-    private void MoveToStartPosition()
+public enum NinjutsuName
+{
+    Fireball,
+    Raikiri,
+    GalePalm,
+    RockShelterCollapse,
+    Waterfall,
+    PhoenixFire,
+    Rasengan
+}
+
+public enum ElementType
+{
+    Fire,
+    Water,
+    Wind,
+    Earth,
+    Lightning,
+    None
+}
+
+public struct Ninjutsu
+{
+    public NinjutsuName name;
+    public int damage;
+    public ElementType element;
+
+    public Ninjutsu(NinjutsuName name, int damage, ElementType element)
     {
-        
+        this.name = name;
+        this.damage = damage;
+        this.element = element;
     }
 }
